aa3e61292caabc787b0fdc6c23a00feb2a966acf
Devan Kestel <devan.kestel@gmail.com>
2017-08-19 23:57:23 -0400

    Working build but need to fix model to properly relate Category and Task lists.

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,120 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
--- Main
+-- import Time.DateTime as DateTime exposing (DateTime, dateTime)
+-- MAIN
 
 
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
-        { model = model
+        { model = initModel
         , view = view
         , update = update
         }
 
 
 
--- Model
--- Update
--- View
+-- MODEL
+
+
+type alias Category =
+    { name : String
+    , pointValue : Int
+    }
+
+
+type alias Completion =
+    { isCompleted : Bool
+    , dateCompleted : Int
+    }
+
+
+type alias Task =
+    { title : String
+    , category : Category
+    , completion : Completion
+    }
+
+
+type alias Model =
+    { categories : List Category
+    , tasks : List Task
+    }
+
+
+categories : List Category
+categories =
+    [ { name = "Dragon"
+      , pointValue = 1
+      }
+    , { name = "Mermaid"
+      , pointValue = 3
+      }
+    , { name = "Maze"
+      , pointValue = 10
+      }
+    ]
+
+
+tasks : List Task
+tasks =
+    [ { title = "This is the first task. Dragons."
+      , category = { name = "Dragon", pointValue = 1 }
+      , completion = { isCompleted = False, dateCompleted = 0 }
+      }
+    , { title = "This is the second task. Mermaids."
+      , category = { name = "Mermaid", pointValue = 2 }
+      , completion = { isCompleted = False, dateCompleted = 0 }
+      }
+    , { title = "This is the third task. Maze."
+      , category = { name = "Maze", pointValue = 10 }
+      , completion = { isCompleted = False, dateCompleted = 0 }
+      }
+    ]
+
+
+initModel : Model
+initModel =
+    { categories = categories
+    , tasks = tasks
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Clear ->
+            initModel
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        header =
+            ("Tasks")
+    in
+        div
+            [ style
+                [ ( "font-family", "Helvetica" )
+                , ( "font-size", "40px" )
+                , ( "margin-left", "40px" )
+                ]
+            ]
+            [ h1 [] [ text header ]
+            ]
